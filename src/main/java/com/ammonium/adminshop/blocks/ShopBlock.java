35661fed3ad9151484523fb2727c54cd4a5a28b9
AmmoniumX <diego.garcia.cr@gmail.com>
2023-07-10 14:33:29 -0600

    Added block drops

--- src/main/java/com/ammonium/adminshop/blocks/ShopBlock.java
+++ src/main/java/com/ammonium/adminshop/blocks/ShopBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EntityBlock;
@@ -21,6 +22,7 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -28,6 +30,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.NetworkHooks;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class ShopBlock extends CustomDirectionalBlock implements EntityBlock {
     public static final String MESSAGE_SHOP = "message.shop";
@@ -43,6 +46,11 @@ public class ShopBlock extends CustomDirectionalBlock implements EntityBlock {
                 .dynamicShape()
                 .noOcclusion()
         );
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState pState, LootContext.Builder pBuilder) {
+        return super.getDrops(pState, pBuilder);
     }
 
     @Override
